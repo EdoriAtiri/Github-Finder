6a5202e8003f60134f362427ef925534bfa417ab
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2022-02-23 14:51:12 +0100

    added get user functiona and alert context

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,6 +9,7 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
+    user: {},
     loading: false,
   }
 
@@ -37,6 +38,27 @@ export const GithubProvider = ({ children }) => {
     })
   }
 
+  //  Get Single User
+  const getUser = async (login) => {
+    setLoading()
+    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    })
+
+    if (response.status === 404) {
+      window.location = '/notfound'
+    } else {
+      const data = await response.json()
+
+      dispatch({
+        type: 'GET_USER',
+        payload: data,
+      })
+    }
+  }
+
   // SetLoading
   const setLoading = () => dispatch({ type: 'SET_LOADING' })
 
@@ -48,8 +70,10 @@ export const GithubProvider = ({ children }) => {
       value={{
         users: state.users,
         loading: state.loading,
+        user: state.user,
         searchUsers,
         clearUsers,
+        getUser,
       }}
     >
       {children}
